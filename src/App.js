ac6024e0f70485725b553f07c42cbfc58729b2c4
unknown <sakshi@trihund.com>
2019-02-04 16:07:32 +0530

    added AttributeModalSelect component and SelectOptions component which uses react hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Button } from 'antd';
+import AttributeModalSelect from './components/AttributeModalSelect';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const [show, updateShow] = React.useState(false);
+  const clickHandler = () => {
+    updateShow(!show);
   }
+  return (
+    <div>
+      <Button type="primary" onClick={clickHandler}>
+        Open Modal
+      </Button>
+      <AttributeModalSelect
+        isOpen={show}
+        options={['L', 'S', 'M', 'XL', 'XLL']}
+        shouldCloseOnSelect={true}
+        onValueSelect={i => updateShow(false)}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SelectOptions.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Icon } from 'antd';
+
+const SelectOptions = ({ defaultSelected, options, onSelect }) => {
+	const [selected, updateSelected] = React.useState(defaultSelected);
+	const selectHandler = i => {
+		if (onSelect) onSelect(i);
+		updateSelected(i);
+	};
+	return (
+		options.map((item, i) => (
+			<div key={i} style={{ position: 'inline', width: 100 }}>
+				<p
+					className={selected === i ? 'selected' : "modal-items"}
+					onClick={() => selectHandler(i)}
+				>
+					{item}
+					<span>
+						{selected === i ? <Icon type='check' style={{ float: 'right', marginTop: 3 }} className={selected === i ? 'selected' : "modal-items"} /> : null}
+					</span>
+				</p>
+			</div>
+		))
+	)
+};
+
+export default SelectOptions;
